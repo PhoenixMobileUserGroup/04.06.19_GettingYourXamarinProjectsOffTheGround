88d3c8acd483ee598e42b83c5ab0a6d8168984cc
Shane Foley <Shane.Foley@neudesic.com>
2019-03-21 18:41:44 -0700

    base view model logic started

---
+++ ProjectSpartan/ProjectSpartan/ProjectSpartan/ViewModels/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using PropertyChanged;
+using System.Threading.Tasks;
+
+namespace ProjectSpartan.UI.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public abstract class BaseViewModel
+    {
+        public string Title { get; set; }
+        public bool IsBusy { get; set; }
+        public Task OnAppearing() { return Task.CompletedTask; }
+        public Task OnDisappearing() { return Task.CompletedTask; }
+    }
+}
