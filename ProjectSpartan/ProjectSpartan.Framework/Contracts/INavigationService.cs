abce11ce99010b659b460a1c44de29cdbae54585
Shane Foley <Shane.Foley@neudesic.com>
2019-04-03 19:33:23 -0700

    navigational interface added, implementation required

---
+++ ProjectSpartan/ProjectSpartan.Framework/Contracts/INavigationService.cs
@@ -0,0 +1,15 @@
+﻿using ProjectSpartan.Framework.UI;
+using System.Threading.Tasks;
+
+namespace ProjectSpartan.Framework.Contracts
+{
+    public interface INavigationService
+    {
+        Task InitializeAsync();
+        Task NavigateToAsync<TViewModel>() where TViewModel : BaseViewModel;
+        Task NavigateToAsync<TViewModel>(object parameter) where TViewModel : BaseViewModel;
+        Task RemoveLastFromBackStackAsync();
+        Task RemoveBackStackAsync();
+        Task BackAsync();
+    }
+}
